e42efa4d6ea759b4a5699945bafac5d8e9512d10
Dosto4k <dudcanos@yandex.ru>
2026-01-24 01:43:12 +0300

    fix(shortener): fix url code generation in admin panel
    Fixed an issue where the url code was not generated when creating a
    short url in the admin panel, instead an empty url code was added

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
+from django.forms import ModelForm
+from django.http import HttpRequest
 
 from shortener.models import ShortURL
+from shortener.services import generate_unique_code
 
 
 @admin.register(ShortURL)
 class ShortUrlAdmin(admin.ModelAdmin):
-    list_display = ["url", "code", "owner"]
+    list_display = ["id", "url", "code", "owner"]
     list_select_related = ["owner"]
+
+    def save_model(
+        self, request: HttpRequest, obj: ShortURL, form: ModelForm, change: bool
+    ) -> None:
+        """Генерирует url code, если он отсутствует"""
+        if obj.code is None or obj.code == "":
+            obj.code = generate_unique_code()
+        return super().save_model(request, obj, form, change)
